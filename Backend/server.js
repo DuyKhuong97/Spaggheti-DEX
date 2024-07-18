cdbccb3711c91c9cf748184b31a30457931ef67d
DuyKhuong97 <duykhuong9a@gmail.com>
2024-07-19 00:31:30 +0700

    update sever

--- Backend/server.js
+++ Backend/server.js
@@ -5,8 +5,11 @@ const app = express();
 const port = 5001;
 
 app.use(cors());
-
 app.use('/artifacts', express.static(path.join(__dirname, 'Artifacts')));
+
+app.get('/artifacts/spagghetiDEX.json', (req, res) => {
+  res.sendFile(path.join(__dirname, 'Artifacts', 'spagghetiDEX.json'));
+});
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
